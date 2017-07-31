8474e4932e198ae48532b8fb9fe0b5c4453167db
Mark Eschbach <meschbach@gmail.com>
2017-07-30 22:21:28 -0700

    System wide tests for registration of a service and utilization of the service

--- lib/mee/delta.rb
+++ lib/mee/delta.rb
@@ -1,6 +1,62 @@
+require 'rest-client'
 require "mee/delta/version"
 
 module MEE
   module Delta
+		@default_url = "http://localhost:9000"
+
+		#TODO: Figure out how to make this a module attribute accesor
+		def self.default_url ; @default_url ; end
+		def self.default_url=( url ) ; @default_url = url ; end
+
+		def self.is_available
+			begin
+				response = HTTPClient::get_json( @default_url + "/v1/status" )
+				response["ok"]
+			rescue
+				false
+			end
+		end
+
+		def self.ingress( listener_name, target_name )
+			result = HTTPClient::post_json( @default_url + "/v1/ingress", { :name => listener_name, :target => target_name, :wire_proxy => "node-http-proxy" }, 201 )
+			return IngressResource.new( result[ '_self' ] )
+		end
+
+		def self.register_target_port( target_name, port )
+			puts "Registering target: #{port} for #{target_name} "
+			response_body = HTTPClient::post_json( @default_url + "/v1/target/" + target_name, { :port => port }, 201 )
+			puts "Response body: #{ response_body }"
+		end
+
+		class IngressResource
+			def initialize( url )
+				@url = url
+			end
+
+			def address
+				json = HTTPClient.get_json( @url )
+				json['address']
+			end
+
+			def add_target( target )
+				HTTPClient::post_json( @url, { :add_targets => [ target ] } )
+			end
+		end
+
+		module HTTPClient
+			def self.get_json( url )
+				result = RestClient.get url, :accept => :json
+				raise "Got status " + result.code.to_s + " for " + url unless result.code == 200
+				JSON.parse( result.body )
+			end
+
+			def self.post_json( url, payload, expected_code = 200 )
+				result = RestClient.post url, payload.to_json, :accept => :json, :content_type => :json
+
+				raise "Got status " + result.code.to_s + " for " + url unless result.code == expected_code
+				JSON.parse( result.body )
+			end
+		end
   end
 end
--- spec/delta/delta_spec.rb
+++ spec/delta/delta_spec.rb
@@ -1,11 +1,96 @@
 require 'spec_helper'
 
+require 'sinatra/base'
+require 'sinatra/json'
+
+class SimpleTestApplication < Sinatra::Base
+	before do
+		unless @registered
+			#MEE::Delta::register_target_port( "ruby-target", 5000 )
+			@registered = true
+		end
+	end
+
+	get '/get-test' do
+		result = { :sinatra => true }
+		json result
+	end
+end
+
+def random_ephemeral_port()
+	base_of_range = 16384
+	port = Random.new.rand( 5000 ) + base_of_range
+	port
+end
+
 describe MEE::Delta do
   it 'has a version number' do
     expect(MEE::Delta::VERSION).not_to be nil
   end
 
-#  it 'verifies the the service is available' do
-#		expect( MEE::Delta.is_available ).to eq( true )
-#  end
+	describe "when the service isn't available" do
+	  it 'reports the service as unavailable' do
+			expect( MEE::Delta.is_available ).to eq( false )
+	  end
+	end
+
+	describe "given the service is available" do
+		# TODO: Make this before/after each
+		before(:all) do
+			port = random_ephemeral_port()
+			puts "Starting"
+			@pid = Kernel.spawn( "cd ../delta && node service.js --ttl 30 --port #{port}" )
+			MEE::Delta.default_url = "http://localhost:#{port}"
+			# Wait for up to 1 second for the service to come on-line
+			sleep 0.5
+		end
+
+		# TOOD: Not being called on failure
+		after(:all) do
+			puts "Kill subproc"
+			sleep 0.1
+			begin
+				Process.kill( "TERM", @pid )
+				puts "Term sleep"
+				sleep 1
+				Process.wait( @pid )
+			rescue
+				puts "WARNING: May have leaked process #{@pid}"
+			end
+		end
+
+	  it 'reports the service as available' do
+			expect( MEE::Delta.is_available ).to eq( true )
+	  end
+
+		describe "when a service is registered to a target" do
+			before do
+				@ingress = MEE::Delta.ingress( "ruby-ingress", "ruby-target" )
+				@ingress.add_target( "ruby-target" )
+				@ingress_url = @ingress.address
+				puts "Ingress URL: #{@ingress_url}"
+				@service = fork {
+					port = Random.new.rand( 5000 ) + 32000
+					MEE::Delta.register_target_port( "ruby-target", port )
+					puts "Simple test application registered on #{ port }"
+
+					SimpleTestApplication.run! :port => port
+				}
+				puts "Forked service as PID #{@service}"
+				# Wait until the process is booted
+				sleep 10
+				puts "Simple service should be running"
+			end
+
+			after do
+				Process.kill( "INT", @service ) if @service
+			end
+
+			it "is able to access the service" do
+				response = MEE::Delta::HTTPClient.get_json( @ingress_url + "/get-test" )
+				expect( response["sinatra"] ).to be(true)
+			end
+		end
+	end
 end
+
